bf6e8094273a7811919b8b2ef3c5514fce4ba6dd
Lilith Lamber <winter.rituel@gmail.com>
2021-12-27 13:55:34 +0200

    basic layout

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -5,3 +5,36 @@
 2) Надписи на кнопке старт/стоп меняются
 3) Сохранять состояние counter в localStorage
 */
+
+import {Component} from "react";
+
+class Timer extends Component {
+    state = {
+        count: 0,
+        isActive: false
+    }
+
+    start = () => {
+        setInterval(() => this.setState({count: this.state.count + 1}), 1000)
+    }
+
+    reset = () => {
+        this.setState({count: 0, isActive: false})
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div className='timer'>
+                    <span className='timer_count'>{this.state.count}</span>
+                    <div className="timer_controls">
+                        <button onClick={this.start}>{this.state.isActive ? 'Pause' : 'Start'}</button>
+                        <button onClick={this.reset}>Reset</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Timer;
